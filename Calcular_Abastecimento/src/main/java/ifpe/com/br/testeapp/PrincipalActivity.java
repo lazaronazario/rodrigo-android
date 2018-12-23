5f3c72b545b1d32822811c711d22334c349bd119
Rodrigo Gomes de Souza Costa <rodrigogomes8@gmail.com>
2018-12-23 03:43:57 -0300

    teste

---
+++ Calcular_Abastecimento/src/main/java/ifpe/com/br/testeapp/PrincipalActivity.java
@@ -0,0 +1,58 @@
+package ifpe.com.br.testeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static ifpe.com.br.testeapp.R.id.BTNOK;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+
+    EditText editTextGas ;
+    EditText editTextAlco;
+    TextView  textResult;
+    Button BTNOK;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+      editTextGas = (EditText) findViewById(R.id.editTextGas);
+      editTextAlco = (EditText) findViewById(R.id.editTextAlco);
+      textResult  = (TextView) findViewById(R.id.textResult);
+
+
+      BTNOK = (Button) findViewById(R.id.BTNOK);
+
+
+      BTNOK.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+              double gasolina = Double.parseDouble(editTextGas.getText().toString());
+              double alcool = Double.parseDouble(editTextAlco.getText().toString());
+
+              String Resultado = "Melhor opção foi:" ;
+
+              if((gasolina * 0.7)> alcool){
+                  Resultado = "Àlcool";
+
+              }else{
+                  Resultado = "Gasolina";
+
+              }
+              textResult.setText(Resultado);
+          }
+      });
+
+
+
+    }
+}
